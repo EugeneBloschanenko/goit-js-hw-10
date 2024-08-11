6a9e1d1bfc2ab24945989c1c72acb393c25e9c01
Eugene <Bloschanenko@gmail.com>
2024-08-11 22:49:05 +0300

    timer
    changes

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -16,3 +16,24 @@ const options = {
 };
 
 flatpickr('#datetime-picker', options);
+
+const startBtn = document.querySelector('[data-start]');
+startBtn.addEventListener('click', () => {
+  const initTime = Date.now(); 
+  setInterval(() => {
+    const currentTime = Date.now();
+    const diff = currentTime - initTime;
+  let h,m,s;
+  h = Math.floor(diff/1000/60/60);
+  m = Math.floor((diff/1000/60/60 - h)*60);
+  s = Math.floor(((diff/1000/60/60 - h)*60 - m)*60);  
+  s < 10 ? s = `0${s}`: s = `${s}`
+  m < 10 ? m = `0${m}`: m = `${m}`
+  h < 10 ? h = `0${h}`: h = `${h}`
+  console.log(`${h}:${m}:${s}`);
+  }, 1000)
+  
+
+
+  
+});

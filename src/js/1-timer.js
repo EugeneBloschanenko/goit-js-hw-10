a541d9505e149d31d0036b117e025a4d0af1dc7b
Eugene <Bloschanenko@gmail.com>
2024-08-19 12:45:32 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -18,22 +18,22 @@ const options = {
 flatpickr('#datetime-picker', options);
 
 const startBtn = document.querySelector('[data-start]');
+
 startBtn.addEventListener('click', () => {
-  const initTime = Date.now(); 
+  const currentTime = Date.now();
+  const initTime = currentTime + 2629800000; 
   setInterval(() => {
     const currentTime = Date.now();
-    const diff = currentTime - initTime;
-  let h,m,s;
-  h = Math.floor(diff/1000/60/60);
-  m = Math.floor((diff/1000/60/60 - h)*60);
-  s = Math.floor(((diff/1000/60/60 - h)*60 - m)*60);  
+    const diff = initTime - currentTime;
+    let d, h, m, s;
+  d = Math.floor(diff / (1000 * 60 * 60 * 24));
+  h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  s = Math.floor((diff % (1000 * 60)) / 1000);  
   s < 10 ? s = `0${s}`: s = `${s}`
   m < 10 ? m = `0${m}`: m = `${m}`
-  h < 10 ? h = `0${h}`: h = `${h}`
-  console.log(`${h}:${m}:${s}`);
-  }, 1000)
-  
-
-
-  
+  h < 10 ? h = `0${h}` : h = `${h}`
+  d < 10 ? d = `0${d}`: d = `${d}`
+  console.log(`${d}:${h}:${m}:${s}`);
+  }, 1000) 
 });
